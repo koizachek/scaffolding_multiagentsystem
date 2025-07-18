2a8b334bec885569b9817e8da1b4b18b12bdfdcc
PygmalesDev <uk084094@student.uni-kassel.de>
2025-07-18 18:40:44 +0200

    initialized web application for editing concept maps; nodes and edges can be created and dragged

---
+++ MAS/webapp/webapp.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+class WebApplication:
+    def __init__(self):
+        self._app = Flask(__name__)
+        
+        self._app.add_url_rule("/", view_func=self.index)
+
+    @staticmethod
+    def index():
+        return render_template("index.html")
+    
+
+    def run(self):
+        self._app.run(debug=True)
+
+
+if __name__ == "__main__":
+    webapp = WebApplication()
+    webapp.run()
